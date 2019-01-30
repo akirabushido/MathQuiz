2bbb079f006a1ef7d05ef15067d3dc888028ee35
Omar Gonzalez <32150067+akirabushido@users.noreply.github.com>
2019-01-30 11:20:11 +0900

    This is a quick math quiz
    This Math quiz asks you random math questions(multiplication and addition) and times how long it takes to take the quiz and also it tracks how many you got correct or wrong. This was mostly done with the help of Team TreeHouse. The purpose was to use datetime.

---
+++ TTHMathQuizApp/TTHMathQuiz.py
@@ -0,0 +1,33 @@
+import datetime
+import random
+
+from questions import Add, Multiply
+
+
+class Quiz:
+    questions = []
+    answer = []
+    
+    def __init__(self):
+        #generate 10 random questions with  numbers from 1 - 10
+        #add these questions into self.questions
+    
+    def take_quiz(self):
+        #log the start time
+        #ask all the questions
+        #log if they got the qeustion right
+        #log the end time
+        #show summary
+
+    def ask(self, question):
+        # log the start time
+        #capture the answer
+        #check answer
+        #log end time
+        #if the answer is right send back True
+        #otherwise, send back False
+        #send bback the elapsed time, too
+    
+    def summary(self):
+        #print how many you got right and total number questions 5/10
+        #print total time for the quiz: 30 seconds
